bb7caebe12c2180f39ad12d128b5ff4945627313
Brian McLaren <brianqmclaren@gmail.com>
2026-05-05 11:52:12 -0400

    Add in missing forward slash

--- src/lib/avatars.ts
+++ src/lib/avatars.ts
@@ -4,7 +4,7 @@ export const STAR_AVATAR_MAP: Record<number, string> = {
   246: "/avatars/nikola-jokic.png",
   140: "/avatars/kevin-durant.png",
   237: "/avatars/lebron-james.png",
-  73: "avatars/jalen-brunson.png",
+  73: "/avatars/jalen-brunson.png",
   56677822: "/avatars/victor-wembanyama.png",
   115: "/avatars/steph-curry.png",
   3547238: "/avatars/anthony-edwards.png",
